68751e7d56ae161b44ca14a0179633873a73e099
Da-Ronja <70214836+Da-Ronja@users.noreply.github.com>
2021-05-23 22:43:11 +0200

    POST request for uplodNewHamster with imgName

--- src/components/gallery/UploadNewHamster.jsx
+++ src/components/gallery/UploadNewHamster.jsx
@@ -1,10 +1,11 @@
-// [ ] Upload img with validation
 // [ ] css + css för att förtydliga inputfälten
 // [ ] tydliga felmedelanden vid fel inmatning
 // [ ] validering av siffror så att den inte kan gå under 0
 // [ ] Validerinf av fetch POST request
+// [ ] Error message on input img 
 
 //QUESTION onSubmit on button or on  form
+//QUESTION Varför får jag fel meddelande av input file
 //QUESTION Till Sara Ska det vara input= text eller textarea??
 
 import { useState } from "react"
@@ -12,11 +13,30 @@ import { useState } from "react"
 
 const UploadNewHamster = () => {
 
+	//const fileInput = useRef(null)
 	const [name, setName] = useState('')
 	const [age, setAge] = useState(0)
 	const [favFood, setFavFood] = useState('')
 	const [loves, setLoves] = useState('')
+	//const [ingFile, setIngFile] = useState(null)
+	const [imgName, setImgName] = useState('')
 	//const history = useHistory()
+
+	const handleFileInput = (e) => {
+		// handle validations
+		const file = e.target.files[0];
+		console.log(file);
+
+		if (file.size > 1024) {
+			console.log('File is to big');
+		} else {
+			//setIngFile(file)
+			let fileName = file.name
+			setImgName(fileName)
+			console.log('Size is good');
+		}
+	}
+
 
 	const handleSubmit = (event) => {
 		event.preventDefault()
@@ -29,7 +49,7 @@ const UploadNewHamster = () => {
 			wins: 0,
 			defeats: 0,
 			games: 0,
-			imgName: "test.jpg"
+			imgName
 		};
 
 		fetch('http://localhost:1357/hamsters/', {
@@ -46,7 +66,7 @@ const UploadNewHamster = () => {
 		console.log(event.target.value);
 		//NOTE validateAge
 	}
-
+	//NOTE Uncaught DOMException: Failed to execute 'getRangeAt' on 'Selection': 0 is not a valid index.
 	return (
 		<div>
 			<h2>Upload New Hamster</h2>
@@ -79,6 +99,13 @@ const UploadNewHamster = () => {
 					required
 					value={loves}
 					onChange={(event) => setLoves(event.target.value)}
+				/>
+				{/* https://dev.to/faddalibrahim/filtering-and-validating-file-uploads-with-javascript-327p 
+				https://codeforgeek.com/file-uploads-using-node-js/*/}
+				<input
+					type="file"
+					multiple accept=".jpg, .png"
+					onChange={handleFileInput}
 				/>
 				<button>Upload Hamster</button>
 			</form>
